c9f8c200154c1d400f91a6bc4317715c78bce412
Satepin <61434783+satepin@users.noreply.github.com>
2026-09-07 01:26:13 -0300

    base de login, componente reutilizable de login (#6)

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import { homeController } from '../controllers/homeController';
+import { loginRouter } from './login';
 
 const router = Router();
 
 router.get('/', homeController);
+router.use(loginRouter);
 
 export { router };
