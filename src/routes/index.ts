8414daced9b3519c9384f9a1af738f2bba1eb57e
Mateo <teoastudy@gmail.com>
2026-09-08 19:27:45 -0300

    Revert "base de login, componente reutilizable de login (#6)" (#9)
    This reverts commit c9f8c200154c1d400f91a6bc4317715c78bce412.

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,8 @@
 import { Router } from 'express';
 import { homeController } from '../controllers/homeController';
-import { loginRouter } from './login';
 
 const router = Router();
 
 router.get('/', homeController);
-router.use(loginRouter);
 
 export { router };
