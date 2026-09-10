6cebad159751e6815f0ea6473d41e3cf10d2aaa7
Satepin <salva2rterranova@gmail.com>
2026-09-10 16:56:58 -0300

    carrito con productos constantes 💀

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express';
+import { cartController, checkoutController } from '../controllers/cartController';
 import { homeController } from '../controllers/homeController';
 import { loginRouter } from './login';
 
 const router = Router();
 
 router.get('/', homeController);
+router.get('/cart', cartController);
+router.get('/checkout', checkoutController);
 router.use(loginRouter);
 
 export { router };
