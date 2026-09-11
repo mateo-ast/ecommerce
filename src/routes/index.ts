fbd175abf850086ba52ef7442945c7506a17829e
Satepin <salva2rterranova@gmail.com>
2026-09-11 17:36:56 -0300

    Cart scope only

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,8 +2,6 @@ import { Router } from 'express';
 import { cartController, checkoutController } from '../controllers/cartController';
 import { homeController } from '../controllers/homeController';
 import { loginRouter } from './login';
-import { registerRouter } from './register';
-import { checkoutRouter } from './checkout';
 
 const router = Router();
 
@@ -11,7 +9,5 @@ router.get('/', homeController);
 router.get('/cart', cartController);
 router.get('/checkout', checkoutController);
 router.use(loginRouter);
-router.use(registerRouter);
-router.use(checkoutRouter);
 
 export { router };
