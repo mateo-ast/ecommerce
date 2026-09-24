dee298312b166ce90dd7ab70965ed7c56c89a8b2
Mateo <teoastudy@gmail.com>
2026-09-24 15:17:40 -0300

    mostOrderedProducts (#69)

--- src/controllers/homeController.ts
+++ src/controllers/homeController.ts
@@ -1,8 +1,21 @@
 import { Request, Response } from 'express';
 import { JSONProductModel } from '../models/JSONProductModel';
+import { ProductModel } from '../models/productsModel';
+
+// TODO Move to productService
+async function getMostOrderedProducts(productModel: ProductModel) {
+  return (await productModel.getAll()).splice(20, 10);
+}
 
 export const getHome = async (_req: Request, res: Response) => {
   const productModel = new JSONProductModel();
+  // TODO pagination
   const products = await productModel.getAll();
-  res.render('pages/index', { title: 'Home', products });
+  const mostOrderedProducts = await getMostOrderedProducts(productModel);
+
+  res.render('pages/index', {
+    title: 'Home',
+    products,
+    mostOrderedProducts,
+  });
 };
